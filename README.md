e92fc4e078b5bfb0e2f32b48c4f5764cf2b8e27d
apella1 <johncollons5@gmail.com>
2024-05-23 12:48:29 +0300

    setup: add project yml configs and dependencies

---
+++ README.md
@@ -0,0 +1,10 @@
+# Zima Spring Backend
+
+## Technologies Used
+
+- Spring Boot 3
+- Spring Security 6
+- JWT Token Authentication
+- Spring Data JPA
+- JSR-303 and Spring Validation
+- GitHub Actions
---
+++ src/test/java/me/apella/album/PhotoAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package me.apella.album;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAlbumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
